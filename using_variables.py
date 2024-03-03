7829eb1d9557c7c6e5bbd02310ffeb2f4999a8fd
theparadox07 <theunknownparadox07@gmail.com>
2024-03-03 09:04:08 +0530

    change_dir

---
+++ using_variables.py
@@ -0,0 +1,11 @@
+import subprocess
+
+interface = "Wlan0"
+new_mac = "00:11:22:33:44:77"
+
+
+# subprocess.call("ifconfig wlan0 down",shell=True)
+# subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66",shell=True)
+# subprocess.call("ifconfig wlan0 up",shell=True)
+
+print("[+] Changing MAC adress for "+ interface+" to "+ new_mac)
