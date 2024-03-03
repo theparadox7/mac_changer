199139572a646bfb5c87af1f3173e26038f5c9fd
theparadox07 <theunknownparadox07@gmail.com>
2024-03-03 09:14:08 +0530

    fixed some errors

--- using_variables.py
+++ using_variables.py
@@ -1,7 +1,7 @@
 import subprocess
-interface = "Wlan0"
+interface = "wlan0"
 new_mac = "00:11:22:33:44:77"
 print("[+] Changing MAC adress for "+ interface+" to "+ new_mac)
-subprocess.call("ifconfig "+interface+ "down",shell=True)
-subprocess.call("ifconfig "+interface+ "hw ether "+ new_mac, shell=True)
-subprocess.call("ifconfig "+interface+ "up",shell=True)
+subprocess.call("ifconfig "+interface+ " down ",shell=True)
+subprocess.call("ifconfig "+interface+ " hw ether "+ new_mac, shell=True)
+subprocess.call("ifconfig "+interface+ " up",shell=True)
